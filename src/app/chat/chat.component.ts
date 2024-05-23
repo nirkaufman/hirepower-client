b61f12d03a53d915204caeb8f5874103f4ab6cc1
nirkaufman <nirkaufman@gmail.com>
2024-05-23 16:13:41 +0300

    implement chat component

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,4 +1,4 @@
-import {Component, inject} from "@angular/core";
+import {AfterViewInit, Component, ElementRef, inject, OnDestroy, ViewChild} from "@angular/core";
 import {ChatService} from "./chat.service";
 import {toSignal} from "@angular/core/rxjs-interop";
 import {NgClass} from "@angular/common";
@@ -7,6 +7,7 @@ import {NgClass} from "@angular/common";
 @Component({
   selector: 'hp-chat',
   standalone: true,
+  imports: [NgClass],
   template: `
     <div class="flex flex-col justify-between h-screen">
       <div class="overflow-auto p-4" #chatContainer>
@@ -27,15 +28,14 @@ import {NgClass} from "@angular/common";
                placeholder="Ask me anything..."
                (keydown.enter)="handleUserPrompt($event)">
       </div>
-
     </div>
   `,
-  imports: [
-    NgClass
-  ]
 })
-export class ChatComponent {
+export class ChatComponent implements AfterViewInit, OnDestroy {
   private readonly chatService = inject(ChatService);
+  private mutationObserver: MutationObserver;
+
+  @ViewChild('chatContainer') private chatContainer: ElementRef;
 
   chatResponse = toSignal(this.chatService.chatResponse$, { initialValue: [] });
 
@@ -43,6 +43,22 @@ export class ChatComponent {
     const inputElement = event.target as HTMLInputElement;
     this.chatService.updatePrompt(inputElement.value);
     inputElement.value = '';
+  }
+
+  ngAfterViewInit() {
+    this.mutationObserver = new MutationObserver(() => {
+      this.chatContainer.nativeElement.scrollTop = this.chatContainer.nativeElement.scrollHeight;
+    });
+
+    this.mutationObserver.observe(this.chatContainer.nativeElement, {
+      childList: true,
+      subtree: true
+    });
+  }
+
+  ngOnDestroy() {
+    this.mutationObserver.disconnect();
+    this.chatService.updatePrompt('reset');
   }
 
 }
