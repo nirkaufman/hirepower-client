3dac15450e82d3d6e83ac2ec14d45216ccdb4eba
nirkaufman <nirkaufman@gmail.com>
2024-05-23 16:05:33 +0300

    implement chat component

--- src/app/chat/chat.component.ts
+++ src/app/chat/chat.component.ts
@@ -1,11 +1,48 @@
-import {Component} from "@angular/core";
+import {Component, inject} from "@angular/core";
+import {ChatService} from "./chat.service";
+import {toSignal} from "@angular/core/rxjs-interop";
+import {NgClass} from "@angular/common";
 
 
 @Component({
   selector: 'hp-chat',
   standalone: true,
   template: `
-    <h1>Chat</h1>
+    <div class="flex flex-col justify-between h-screen">
+      <div class="overflow-auto p-4" #chatContainer>
+
+        <ul class="space-y-4">
+          @for (msg of chatResponse(); track msg) {
+            <li class="p-2 rounded-md"
+                [ngClass]="{'bg-blue-200': msg.role === 'user', 'bg-green-200': msg.role === 'assistant'}">
+              {{ msg.role }}:
+              {{ msg.content }}
+            </li>
+          }
+        </ul>
+      </div>
+
+      <div class="p-4 bg-gray-200 sticky bottom-0">
+        <input class="w-full p-2 rounded-md"
+               placeholder="Ask me anything..."
+               (keydown.enter)="handleUserPrompt($event)">
+      </div>
+
+    </div>
   `,
+  imports: [
+    NgClass
+  ]
 })
-export class ChatComponent {}
+export class ChatComponent {
+  private readonly chatService = inject(ChatService);
+
+  chatResponse = toSignal(this.chatService.chatResponse$, { initialValue: [] });
+
+  handleUserPrompt(event: any) {
+    const inputElement = event.target as HTMLInputElement;
+    this.chatService.updatePrompt(inputElement.value);
+    inputElement.value = '';
+  }
+
+}
--- src/app/chat/chat.service.ts
+++ src/app/chat/chat.service.ts
@@ -1,7 +1,29 @@
-import {Injectable} from "@angular/core";
+import {inject, Injectable, signal} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {toObservable} from "@angular/core/rxjs-interop";
+import { switchMap} from "rxjs";
 
+export interface ChatMessage {
+  role: string;
+  content: string | null;
+}
 
-@Injectable({ providedIn: 'root'})
+@Injectable({providedIn: 'root'})
 export class ChatService {
+  private readonly httpClient = inject(HttpClient);
+  private readonly prompt = signal<string>('reset');
 
+  private getResponse(userPrompt: string) {
+    return this.httpClient.post<ChatMessage[]>(`http://localhost:3000/chat`, {message: userPrompt}, {
+      responseType: 'json'
+    });
+  }
+
+  chatResponse$ = toObservable(this.prompt).pipe(
+    switchMap((message) => this.getResponse(message)),
+  );
+
+  updatePrompt(input: string) {
+    this.prompt.set(input);
+  }
 }
